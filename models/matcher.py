e71490d31a2b070e0186c9c72d77fc7970c04344
clarencechen <clarencechenct@gmail.com>
2020-12-13 14:14:00 -0800

    convert hungarian matcher to tf

--- models/matcher.py
+++ models/matcher.py
@@ -2,14 +2,13 @@
 """
 Modules to compute the matching cost and solve the corresponding LSAP.
 """
-import torch
+import tensorflow as tf
 from scipy.optimize import linear_sum_assignment
-from torch import nn
 
 from util.box_ops import box_cxcywh_to_xyxy, generalized_box_iou
 
 
-class HungarianMatcher(nn.Module):
+class HungarianMatcher:
     """This class computes an assignment between the targets and the predictions of the network
 
     For efficiency reasons, the targets don't include the no_object. Because of this, in general,
@@ -31,8 +30,7 @@ class HungarianMatcher(nn.Module):
         self.cost_giou = cost_giou
         assert cost_class != 0 or cost_bbox != 0 or cost_giou != 0, "all costs cant be 0"
 
-    @torch.no_grad()
-    def forward(self, outputs, targets):
+    def __call__(self, outputs, targets):
         """ Performs the matching
 
         Params:
@@ -41,9 +39,9 @@ class HungarianMatcher(nn.Module):
                  "pred_boxes": Tensor of dim [batch_size, num_queries, 4] with the predicted box coordinates
 
             targets: This is a list of targets (len(targets) = batch_size), where each target is a dict containing:
-                 "labels": Tensor of dim [num_target_boxes] (where num_target_boxes is the number of ground-truth
+                 "id": Tensor of dim [num_target_boxes] (where num_target_boxes is the number of ground-truth
                            objects in the target) containing the class labels
-                 "boxes": Tensor of dim [num_target_boxes, 4] containing the target box coordinates
+                 "bbox": Tensor of dim [num_target_boxes, 4] containing the target box coordinates
 
         Returns:
             A list of size batch_size, containing tuples of (index_i, index_j) where:
@@ -55,31 +53,38 @@ class HungarianMatcher(nn.Module):
         bs, num_queries = outputs["pred_logits"].shape[:2]
 
         # We flatten to compute the cost matrices in a batch
-        out_prob = outputs["pred_logits"].flatten(0, 1).softmax(-1)  # [batch_size * num_queries, num_classes]
-        out_bbox = outputs["pred_boxes"].flatten(0, 1)  # [batch_size * num_queries, 4]
+        out_prob = tf.nn.softmax(tf.reshape(outputs["pred_logits"], [bs * num_queries, -1]), axis=-1)  # [batch_size * num_queries, num_classes]
+        out_bbox = tf.reshape(outputs["pred_boxes"], [bs * num_queries, -1])  # [batch_size * num_queries, 4]
 
         # Also concat the target labels and boxes
-        tgt_ids = torch.cat([v["labels"] for v in targets])
-        tgt_bbox = torch.cat([v["boxes"] for v in targets])
+        tgt_ids = tf.concat([v["id"] for v in targets], 0)
+        tgt_bbox = tf.concat([v["bbox"] for v in targets], 0)
 
         # Compute the classification cost. Contrary to the loss, we don't use the NLL,
         # but approximate it in 1 - proba[target class].
         # The 1 is a constant that doesn't change the matching, it can be ommitted.
-        cost_class = -out_prob[:, tgt_ids]
+        cost_class = -tf.gather(out_prob, tgt_ids, axis=1)
 
         # Compute the L1 cost between boxes
-        cost_bbox = torch.cdist(out_bbox, tgt_bbox, p=1)
+        cost_bbox = tf.reduce_mean(tf.abs(out_bbox - tgt_bbox), axis=-1)
 
         # Compute the giou cost betwen boxes
         cost_giou = -generalized_box_iou(box_cxcywh_to_xyxy(out_bbox), box_cxcywh_to_xyxy(tgt_bbox))
 
         # Final cost matrix
         C = self.cost_bbox * cost_bbox + self.cost_class * cost_class + self.cost_giou * cost_giou
-        C = C.view(bs, num_queries, -1).cpu()
+        C = tf.reshape(bs, num_queries, -1)
 
-        sizes = [len(v["boxes"]) for v in targets]
-        indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
-        return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
+        sizes = [len(v["bbox"]) for v in targets]
+
+        offset, indices = 0, []
+        for i, c in enumerate(sizes):
+            src_idxs, tgt_idxs = linear_sum_assignment(C[i, :, offset:offset + c]))
+            indices.append((tf.Variable(src_idxs, dtype=tf.int64, trainable=False), 
+                     tf.Variable(tgt_idxs, dtype=tf.int64, trainable=False)))
+            offset += c
+
+        return indices
 
 
 def build_matcher(args):
