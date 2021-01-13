7f10fbdd7d1dbcd953544d3c73aa0e360f971ce3
clarencechen <clarencechenct@gmail.com>
2021-01-12 17:26:02 -0800

    vectorize additional dimensions for box_iou

--- util/box_ops.py
+++ util/box_ops.py
@@ -32,13 +32,14 @@ def box_xyxy_to_cxcywh(x):
 def box_iou(x1, x2):
     a1, a2 = box_area_xyxy(x1), box_area_xyxy(x2)
 
-    lt = tf.maximum(tf.expand_dims(x1[:, :2], -2), x2[:, :2])  # [N,M,2]
-    rb = tf.minimum(tf.expand_dims(x1[:, 2:], -2), x2[:, 2:])  # [N,M,2]
+    x1_exp, x2_exp = tf.expand_dims(x1, -2), tf.expand_dims(x2, -3)
+    lt = tf.maximum(x1_exp[..., :2], x2_exp[..., :2])  # [...,N,M,2]
+    rb = tf.minimum(x1_exp[..., 2:], x2_exp[..., 2:])  # [...,N,M,2]
 
-    wh = tf.maximum(rb - lt, 0)  # [N,M,2]
-    inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
+    wh = tf.maximum(rb - lt, 0)  # [...,N,M,2]
+    inter = wh[..., 0] * wh[..., 1]  # [...,N,M]
 
-    union = tf.expand_dims(a1, -1) + a2 - inter
+    union = tf.expand_dims(a1, -1) + tf.expand_dims(a2, -2) - inter
 
     iou = inter / union
     return iou, union
@@ -50,21 +51,22 @@ def generalized_box_iou(x1, x2):
 
     The boxes should be in [x0, y0, x1, y1] format
 
-    Returns a [N, M] pairwise matrix, where N = len(x1)
-    and M = len(x2)
+    Returns a [..., N, M] pairwise matrix, where N = x1.shape[-1]
+    and M = x2.shape[-1]
     """
     # degenerate boxes gives inf / nan results
     # so do an early check
-    x1_ok = tf.debugging.assert_greater_equal(x1[:, 2:], x1[:, :2])
-    x2_ok = tf.debugging.assert_greater_equal(x2[:, 2:], x2[:, :2])
+    x1_ok = tf.debugging.assert_greater_equal(x1[..., 2:], x1[..., :2])
+    x2_ok = tf.debugging.assert_greater_equal(x2[..., 2:], x2[..., :2])
     with tf.control_dependencies([x1_ok, x2_ok]):
         iou, union = box_iou(x1, x2)
 
-        lt = tf.minimum(tf.expand_dims(x1[:, :2], -2), x2[:, :2])
-        rb = tf.maximum(tf.expand_dims(x1[:, 2:], -2), x2[:, 2:])
+        x1_exp, x2_exp = tf.expand_dims(x1, -2), tf.expand_dims(x2, -3)
+        lt = tf.minimum(x1_exp[..., :2], x2_exp[..., :2])
+        rb = tf.maximum(x1_exp[..., 2:], x2_exp[..., 2:])
 
-        wh = tf.maximum(rb - lt, 0)  # [N,M,2]
-        hull = wh[:, :, 0] * wh[:, :, 1]
+        wh = tf.maximum(rb - lt, 0)  # [...,N,M,2]
+        hull = wh[..., 0] * wh[..., 1]
 
         return iou - (hull - union) / hull
 
